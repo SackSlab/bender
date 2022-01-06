88cc55c70656ed3217c91eb5519bdcd23a8298a1
SackSlab <blassackmann@gmail.com>
2022-01-06 19:22:47 -0300

    feat(bender): init fx app

--- cmd/bender/main.go
+++ cmd/bender/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "go.uber.org/fx"
+
+// TODO: setup fx
+// TODO: setup logger
+// TODO: setup db conn
+// TODO: setup gin server
+func main() {
+	app := fx.New()
+
+	app.Run()
+}
