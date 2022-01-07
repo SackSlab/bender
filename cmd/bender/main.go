f9ed3904a0fc1957a38a21cdb192d87858dd0639
SackSlab <blassackmann@gmail.com>
2022-01-06 21:30:05 -0300

    feat(bender): add db connection

--- cmd/bender/main.go
+++ cmd/bender/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/sackslab/bender/cmd/bender/config"
+	"github.com/sackslab/bender/internal/fxgorm"
 	"github.com/sackslab/bender/internal/logger"
 	"go.uber.org/fx"
 )
 
-// TODO: setup db conn
+// TODO: setup env
 // TODO: setup gin server
 func main() {
 	app := fx.New(
 		logger.RegistModule(),
+		config.RegistModule(),
+		fxgorm.RegistModule(
+			func(c *config.Config) string {
+				return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+					c.DBHost, c.DBUser, c.DBPass, c.DBName)
+			},
+		),
 	)
 
 	app.Run()
